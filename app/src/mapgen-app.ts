596cf886f869a9b5aec08f7a921459fe078a704a
Stefan Valentin <khalilravanna@gmail.com>
2016-12-09 08:23:59 -0600

    Cleaned up some TS errors

--- app/src/mapgen-app.ts
+++ app/src/mapgen-app.ts
@@ -2,7 +2,6 @@ PIXI.SCALE_MODES.DEFAULT = PIXI.SCALE_MODES.NEAREST;
 
 import './polyfills';
 import {GameManager} from './game/game-manager';
-import {Ripple} from './views/ripple';
 
 const rootElement = document.querySelector('.ripple');
 const gameManager = new GameManager(rootElement, 'mapgen');
